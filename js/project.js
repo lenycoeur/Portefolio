41caf1bd823d9f34f3b2ba80938c39e1603e6ddd
lenycoeur <wallshck@gmail.com>
2024-02-13 21:49:48 +0100

    stable mediaq

--- js/project.js
+++ js/project.js
@@ -16,6 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  
+
 
 document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
